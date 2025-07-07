60185a644ea6191d62e5c5d7966468bbac9aeeae
Jonas Andrei <jonas.andrei.calda.ballarta@gmail.com>
2025-07-08 07:25:19 +0800

    Update index.tsx
    - delete unused BrowserRouter import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../Home";
 

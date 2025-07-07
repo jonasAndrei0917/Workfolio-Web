8c1e7a90245f995b3b140c638b0670b422ee7669
Jonas Andrei <jonas.andrei.calda.ballarta@gmail.com>
2025-07-08 07:24:27 +0800

    Update index.tsx
    - change BrowserRouter to HashRouter for 404 error code in github pages deployment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../Home";
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -14,7 +14,7 @@ function AppRoutes() {
           <Route path="/contact" element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

011a178f21474034271cd667b29f389b3b8c293f
Jonas Andrei <jonas.andrei.calda.ballarta@gmail.com>
2025-07-08 08:24:48 +0800

    changes for deployment in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "192.168.100.11",
-    port: 5173,
-  },
+  // server: {
+  //   host: "192.168.100.11",
+  //   port: 5173,
+  // },
   base: "/Workfolio-Web/",
 });
